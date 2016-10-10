3c042afafbaeb0c2b62edf1cfaa58a5a7faf4b92
Justin Yoo <justin.yoo@aliencube.com>
2016-10-11 09:17:45 +1100

    Update pages

---
+++ src/Tennis.WebApp/wwwroot/js/addMatches.js
@@ -0,0 +1,56 @@
+﻿"use strict";
+
+(function ($) {
+    var url = $.validator.format("/competitions/@Model.CompetitionId/teams/{0}/players");
+
+    var emptyOption = function (elementIdPrefix) {
+        var $option = $("<option></option>")
+            .attr("value", "")
+            .text("Select Player");
+        $("select[id^=" + elementIdPrefix + "]").empty().append($option);
+    };
+
+    var appendPlayer = function (elementIdPrefix, player) {
+        var $option = $("<option></option>")
+            .attr("value", player.playerId)
+            .text(player.name);
+        $("select[id^=" + elementIdPrefix + "]").append($option);
+    };
+
+    var appendPlayers = function (elementIdPrefix, players) {
+        $.each(players,
+            function (i, player) {
+                appendPlayer(elementIdPrefix, player);
+            });
+    };
+
+    $("#HomeTeam")
+        .change(function () {
+            emptyOption("home-player-");
+
+            var teamId = $(this).val();
+            if (teamId === "") {
+                return;
+            }
+
+            $.getJSON(url(teamId),
+                function (players) {
+                    appendPlayers("home-player-", players);
+                });
+        });
+
+    $("#AwayTeam")
+        .change(function () {
+            emptyOption("away-player-");
+
+            var teamId = $(this).val();
+            if (teamId === "") {
+                return;
+            }
+
+            $.getJSON(url(teamId),
+                function (players) {
+                    appendPlayers("away-player-", players);
+                });
+        });
+})(jQuery);
---
+++ src/Tennis.WebApp/wwwroot/js/getCompetition.js
@@ -0,0 +1,41 @@
+﻿"use strict";
+
+(function ($) {
+    var url = $.validator.format("/clubs/{0}/teams");
+
+    var emptyOption = function (elementId) {
+        var $option = $("<option></option>")
+            .attr("value", "")
+            .text("Select Team");
+        $("#" + elementId).empty().append($option);
+    };
+
+    var appendTeam = function (elementId, team) {
+        var $option = $("<option></option>")
+            .attr("value", team.teamId)
+            .text(team.name);
+        $("#" + elementId).append($option);
+    };
+
+    var appendTeams = function (elementId, teams) {
+        $.each(teams,
+            function (i, team) {
+                appendTeam(elementId, team);
+            });
+    };
+
+    $("#Club")
+        .change(function () {
+            emptyOption("club-team");
+
+            var clubId = $(this).val();
+            if (clubId === "") {
+                return;
+            }
+
+            $.getJSON(url(clubId),
+                function (teams) {
+                    appendTeams("club-team", teams);
+                });
+        });
+})(jQuery);
